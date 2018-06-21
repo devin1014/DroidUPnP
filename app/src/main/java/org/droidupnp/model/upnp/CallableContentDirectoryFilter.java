4c232797953329ba23d40a40d25999f3ab4a37ec
LIUWEI <wei.liu@neulion.com.cn>
2018-06-21 15:36:36 +0800

    Update: 1.construct project matching android studio

---
+++ app/src/main/java/org/droidupnp/model/upnp/CallableContentDirectoryFilter.java
@@ -0,0 +1,18 @@
+package org.droidupnp.model.upnp;
+
+public class CallableContentDirectoryFilter implements ICallableFilter
+{
+
+    private IUpnpDevice device;
+
+    public void setDevice(IUpnpDevice device)
+    {
+        this.device = device;
+    }
+
+    @Override
+    public Boolean call() throws Exception
+    {
+        return device.asService("ContentDirectory");
+    }
+}
